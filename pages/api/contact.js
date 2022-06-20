c23962265249e2cd5cc3716dba6d6c3a380b70c7
dialaabulkhail <diala.sh.98@gmail.com>
2022-06-20 23:38:36 +0300

    let

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,23 +1,23 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
 export default async (req, res) => {
-  const {name, phone, email, subject, message} = req.body
+  const { name, phone, email, subject, message } = req.body;
 
-  console.log(name, phone, email)
+
   const transporter = nodemailer.createTransport({
-    host:'smtp.gmail.com',
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
       user: process.env.user,
       pass: process.env.pass,
-    }
-  })
-  
-  try{
+    },
+  });
+
+  try {
     const emailRes = await transporter.sendMail({
       from: email,
-      to: 'diala.sh.98@gmail.com',
+      to: "diala.sh.98@gmail.com",
       subject: `contact form submission from ${name}`,
       html: `
       <p>You have a new contact form submission</p><br>
@@ -25,14 +25,13 @@ export default async (req, res) => {
       <p><strong>Phone:</strong> ${phone} </p><br>
       <p><strong>Email:</strong> ${email} </p><br>
       <p><strong>Subject:</strong> ${subject} </p><br>
-      <p><strong>Message:</strong> ${message} </p><br>`
-    })
+      <p><strong>Message:</strong> ${message} </p><br>`,
+    });
 
-    console.log('success!',emailRes.messageId)
-  } catch (err){
-    console.log(err)
+    console.log("success!", emailRes.messageId);
+  } catch (err) {
+    console.log(err);
   }
 
   res.status(200).json(req.body);
-
 };
